195ed36c504a6b33b6d677c0d126989d7d945be7
cereia <104402370+cereia@users.noreply.github.com>
2024-04-12 15:43:03 -0400

    Fixed code so original array isn't destroyed
    Need to decrement while condition to prevent unnecessary iterations

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,13 @@
 def bubble_sort(array)
   sorted = array.sort { |a,b| a <=> b }
-  to_sort = array[0..-1]
+  to_sort = []
+  array.each {|e| to_sort << e.dup}
 
   if to_sort != sorted 
     to_sort.each_with_index do |value, index| 
       increment = 0
       current_val = 0
-      while increment < to_sort.length - 1
+      while increment < to_sort.length
         if index < to_sort.length - 1 
           if to_sort[index] > to_sort[index + 1]    
             current_val = value
@@ -16,14 +17,17 @@ def bubble_sort(array)
         end
         increment += 1
       end
+      # p to_sort
     end
     bubble_sort(to_sort)
   else
+    # p check
+    # p check.all?
     to_sort
   end
 end
 
-p bubble_sort([4, 3, 78, 2, 0, 2])
+ p bubble_sort([4, 3, 78, 2, 0, 2])
 
 # p bubble_sort([3, 4, 5, 6, 6, 7])
 
