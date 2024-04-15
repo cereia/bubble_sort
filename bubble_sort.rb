0726cd3738f3291c3d4103b88d2d642a335d5ce1
cereia <104402370+cereia@users.noreply.github.com>
2024-04-15 12:41:13 -0400

    Remove sort method and stop if array is sorted
    Now checks if array is sorted before recursive call is made

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,34 +1,67 @@
 def bubble_sort(array)
-  sorted = array.sort { |a,b| a <=> b }
+  # create a new array from original to preserve original
   to_sort = []
   array.each {|e| to_sort << e.dup}
 
   length = array.length
-  sorted_array = array.sort { |a, b| a <=> b }
 
-  if to_sort != sorted_array
-    sort_method(to_sort, length)
-  else
+  # create check array of true/false values that will determine if array is sorted or not
+  check = []
+  if is_sorted?(array, check).all?
     to_sort
+  else
+    sort_method(to_sort, length, check)
   end
 end
 
-def sort_method(array, length)
+# sort_method takes an array and swaps the values if the next value is less than the previous
+def sort_method(array, length, check_array)
   i = 0
   cur_val = 0
+  # change the length for each iteration to reduce the iterations each time sort_method is called
   length -= 1
   if length > 0 
-    while i < length
-      if i < length && array[i] > array[i + 1]
+
+    while i < length do
+      if array[i] > array[i + 1]
         cur_val = array[i]
         array[i] = array[i + 1]
         array[i + 1] = cur_val
       end
       i += 1
     end
-    sort_method(array, length)
+
+    # after the iterations, check to see if the array is sorted. Return the array if it is sorted
+    if is_sorted?(array, check_array).all?
+      array
+    else
+      sort_method(array, length, check_array)
+    end
+    
   end
   array
+end
+
+# check through the array to see if the next value is greater than the previous
+def is_sorted?(array, check_array)
+  i = 0
+  while i < array.length - 1 do
+    if array[i] < array[i + 1] || array[i] == array[i + 1]
+      check_array[i] = true
+    else
+      check_array[i] = false
+    end
+    i += 1
+    check_array
+  end
+
+  if i == array.length - 1 && array[i] == array.max
+    check_array[i] = true
+  else
+    check_array[i] = false
+  end
+
+  check_array
 end
 
 p bubble_sort([4, 3, 78, 2, 0, 2])
@@ -36,3 +69,7 @@ p bubble_sort([4, 3, 78, 2, 0, 2])
 # p bubble_sort([3, 4, 5, 6, 6, 7])
 
 # p bubble_sort([32, 45, 33, 6, 232, 4, 2, 56])
+
+# p bubble_sort([5, 1, 4, 2, 8])
+
+# p bubble_sort([2, 4, 3, 5, 6])
